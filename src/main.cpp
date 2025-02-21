8039b47d749ea1d2623409576fbd95e27579e182
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-21 21:27:55 +0330

    function to create a new file

--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,14 @@
 
 using namespace std;
 namespace fs = filesystem;
+
+// Function to create a new file
+void createFile(const string &filename) {
+    ofstream file(filename);  // Open file for writing
+    if (file) {  // If file opened successfully
+        cout << "✅ File " << filename << " created successfully.\n";
+    } else {
+        cout << "❌ Error creating file!\n";  // If there is an error creating the file
+    }
+    file.close();  // Close the file
+}
